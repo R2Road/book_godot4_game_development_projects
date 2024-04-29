c996771331bfbfc25bd7c4d0cd3ba65a05a40f71
R2Road <studior2road@gmail.com>
2024-04-29 13:42:05 +0900

    add script : player

---
+++ chapter_2_coin_dash/player.gd
@@ -0,0 +1,18 @@
+# 모든 스크립트의 첫줄은 어떤 노드를 상속하는지를 나타낸다.
+extends Area2D
+
+
+# Variable
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2( 480, 720 )
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
