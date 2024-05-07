a4faf1aa47fdca4f31b3f8d0740285daface88a0
R2Road <studior2road@gmail.com>
2024-05-07 10:43:23 +0900

    update signal function : coin - _on_area_entered

--- chapter_2_coin_dash/coin.gd
+++ chapter_2_coin_dash/coin.gd
@@ -46,4 +46,5 @@ func _on_timer_timeout():
 
 
 func _on_area_entered(area):
-	pass # Replace with function body.
+	if area.is_in_group( "obstacles" ):
+		position = Vector2( randi_range( 0, screensize.x ), randi_range( 0, screensize.y ) )
