24a60f15c3b7c5a37b1635e9d9da719cc509889b
R2Road <studior2road@gmail.com>
2024-05-28 16:29:42 +0900

    update script : player

--- chapter_3_space_rock/player.gd
+++ chapter_3_space_rock/player.gd
@@ -18,9 +18,22 @@ var rotation_dir = 0
 enum { INIT, ALIVE, INVULNERABLE, DEAD }
 var state = INIT
 
+#
+# override
+#
 func _ready():
 	change_state( ALIVE )
-	
+
+func _process( delta ):
+	get_input()
+
+func _physics_process( delta ):
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+
+#
+#
+#	
 func change_state( new_state ):
 	match new_state:
 		INIT:
@@ -31,3 +44,12 @@ func change_state( new_state ):
 			$CollisionShape2D.set_deferred( "disabled", true )
 		DEAD:
 			$CollisionShape2D.set_deferred( "disabled", true )
+
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed( "thrust" ):
+		thrust = transform.x * engine_power
+	
+	rotation_dir = Input.get_axis( "rotate_left", "rotate_right" )
