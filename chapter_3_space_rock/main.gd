437b8a3ae106407254bf0b8adf8b4f66b4ec199e
R2Road <studior2road@gmail.com>
2024-05-30 15:55:46 +0900

    setup script : main

---
+++ chapter_3_space_rock/main.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+
+
+#
+# export
+#
+@export var rock_scene : PackedScene
+
+
+
+#
+#
+#
+var screensize = Vector2.ZERO
+
+
+
+#
+# override
+#
+func _ready():
+	screensize = get_viewport().get_visible_rect().size
+	for i in 3:
+		spawn_rock( 3 )
+
+
+
+#
+#
+#
+func spawn_rock( size, pos = null, vel = null ):
+	if pos == null:
+		$RockPath/RockSpawn.progress = randi( )
+		pos = $RockPath/RockSpawn.position
+		
+	if vel == null:
+		vel = Vector2.RIGHT.rotated( randf_range( 0, TAU ) ) * randf_range( 50, 125 )
+	
+	var r = rock_scene.instantiate()
+	r.screensize = screensize
+	r.start( pos, vel, size )
+	call_deferred( "add_child", r )
