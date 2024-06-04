3532d1883217028c2d90c5c9eca75bdc6e6e0853
R2Road <studior2road@gmail.com>
2024-06-04 15:17:11 +0900

    add script : enemy

---
+++ chapter_3_space_rock/enemy.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+
+
+#
+#
+#
+@export var bullet_scene : PackedScene
+@export var speed = 150
+@export var rotation_speed = 120
+@export var health = 3
+
+
+
+#
+#
+#
+var follow = PathFollow2D.new()
+
+var target = null
+
+
+
+#
+#
+#
+func _ready():
+	$Sprite2D.frame = randi() % 3
+	var path = $EnemyPaths.get_children()[randi() % $EnemyPaths.get_child_count()]
+	path.add_child( follow )
+	follow.loop = false
